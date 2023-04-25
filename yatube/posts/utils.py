4bcac240e9019b577ed94ccb8b7021192de5a2ea
Sergei Chugunnyi <chugun2010@mail.ru>
2023-04-25 09:55:55 -0400

    Второй коммит

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-
+from django.conf import settings
 
 def get_page_context(request, post_list):
     paginator = Paginator(post_list, settings.POSTS_ON_PAGE)
